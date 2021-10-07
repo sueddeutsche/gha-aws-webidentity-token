193750ba154f4b6bc992209d9703a6e47faa0997
Thomas Seliger <ts@seliger.it>
2021-10-07 15:56:02 +0200

    Debug code

--- scripts/setup_token.sh
+++ scripts/setup_token.sh
@@ -24,8 +24,7 @@ set_github_env_value AWS_ROLE_ARN "${AWS_ROLE_ARN}"
 set_github_env_value AWS_DEFAULT_REGION "${AWS_DEFAULT_REGION}"
 
 [ -n "${DEBUG:-""}" ] && export CURL_DEBUG="-vv"
-curl ${CURL_DEBUG:-} -H "Authorization: bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" "${ACTIONS_ID_TOKEN_REQUEST_URL}" \
-  | jq -r '.value' > "${AWS_WEB_IDENTITY_TOKEN_FILE}"
+curl ${CURL_DEBUG:-} -H "Authorization: bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" "${ACTIONS_ID_TOKEN_REQUEST_URL}" > "${AWS_WEB_IDENTITY_TOKEN_FILE}"
 curl -H "Authorization: bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" "${ACTIONS_ID_TOKEN_REQUEST_URL}" -o -
 cat "${AWS_WEB_IDENTITY_TOKEN_FILE}" | base64
 [ -n "${DEBUG:-""}" ] && cat "${AWS_WEB_IDENTITY_TOKEN_FILE}"
