fdf32e94524717489b86d0ea7e203b54288ba413
Thomas Seliger <ts@seliger.it>
2021-10-07 16:14:45 +0200

    Debug!

--- scripts/setup_token.sh
+++ scripts/setup_token.sh
@@ -27,4 +27,7 @@ set_github_env_value AWS_DEFAULT_REGION "${AWS_DEFAULT_REGION}"
 curl ${CURL_DEBUG:-} -H "Authorization: bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" "${ACTIONS_ID_TOKEN_REQUEST_URL}" > "${AWS_WEB_IDENTITY_TOKEN_FILE}"
 
 cat "${AWS_WEB_IDENTITY_TOKEN_FILE}" | base64
-[ -n "${DEBUG:-""}" ] && cat "${AWS_WEB_IDENTITY_TOKEN_FILE}"
+echo
+echo
+echo
+[ -n "${DEBUG:-""}" ] && cat "${AWS_WEB_IDENTITY_TOKEN_FILE}" | base64
