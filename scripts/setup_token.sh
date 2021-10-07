de297931a801134044830b22d8cfebe8080e892c
Thomas Seliger <ts@seliger.it>
2021-10-07 14:52:43 +0200

    Add debug code and parameter

--- scripts/setup_token.sh
+++ scripts/setup_token.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+[ -n "${DEBUG:-""}" ] && set -x
+
 export AWS_WEB_IDENTITY_TOKEN_FILE=${AWS_WEB_IDENTITY_TOKEN_FILE:-"/tmp/awscreds"}
 export AWS_ROLE_ARN=${AWS_ROLE_ARN:-"not-specified"}
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-"eu-central-1"}
@@ -22,3 +24,5 @@ set_github_env_value AWS_ROLE_ARN "${AWS_ROLE_ARN}"
 set_github_env_value AWS_DEFAULT_REGION "${AWS_DEFAULT_REGION}"
 curl -H "Authorization: bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" "${ACTIONS_ID_TOKEN_REQUEST_URL}" \
   | jq -r '.value' > "${AWS_WEB_IDENTITY_TOKEN_FILE}"
+
+[ -z "${DEBUG:-""}" ] && cat "${AWS_WEB_IDENTITY_TOKEN_FILE}"
